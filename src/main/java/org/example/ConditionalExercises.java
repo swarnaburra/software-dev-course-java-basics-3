985213fe8e04441029f1ea8dc2ceab603087745b
swarnaburra <swarnaburra2025@gmail.com>
2025-12-11 01:52:12 -0600

    Conditional and Loop Exercises

--- src/main/java/org/example/ConditionalExercises.java
+++ src/main/java/org/example/ConditionalExercises.java
@@ -3,23 +3,41 @@ package org.example;
 public class ConditionalExercises {
     public boolean lessThanFive(int x) {
         // Replace the line below with code that returns true if x is less than 5, and false otherwise
-        // (use an if statement with an else block.)
-        return false;
+        if (x < 5) {
+            return true;
+        } else {
+            // (use an if statement with an else block.)
+            return false;
+        }
     }
 
     public String getAgeGroup(int age) {
         // Replace the line below with code that returns "child" if age is less than 13,
+        if (age < 13) {
+            return "child";
+
+        }
         // "teen" if age is less than 20, and "adult" otherwise
-        // (use if, else if, and else)
-        return "";
+        else if (age < 20) {
+            return "teen";
+        } else {
+            return "adult";
+        }
+
     }
 
     public boolean isValidPassword(String password) {
         int passwordLength = password.length();
 
         // Replace the line below with code that returns true if password is at least 8 characters long
+
+        if (passwordLength >= 8) {
+            return true;
+        } else {
+            return false;
+        }
         // and false otherwise
         // (use an if statement with an else block.)
-        return false;
+
     }
 }
--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -1,16 +1,36 @@
 package org.example;
 
 public class LoopExercises {
-    public int sum(int n) {
-        // Replace the line below with code that returns the sum of the numbers from 1 to n
-        // (use a for loop)
-        return 0;
+
+    public static int sum(int n) {
+        int sumOfNumber = 0;
+
+        for (int x = 1; x <= n; x++) {
+            sumOfNumber = sumOfNumber + x;
+        }
+
+        return sumOfNumber;
     }
 
-    public int sumUntilEven(int n) {
-        // Replace the line below with code that returns the sum of the numbers from 1 to n
-        // but stops adding when the sum is even
-        // (use a while loop with a sum variable and a counter variable)
-        return 0;
+    public  static int sumUntilEven(int n) {
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+
+            if (sum % 2 == 0) {
+                break;
+            }
+
+            i++;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args){
+        System.out.println(sum(5));
+        System.out.println(sumUntilEven(5));
     }
 }
